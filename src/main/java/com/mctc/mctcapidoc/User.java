84ed25001c8a46f3cdb35be99a294455001bd759
龚军 <37832790+alvingj@users.noreply.github.com>
2020-07-16 21:17:17 +0800

    修改

--- src/main/java/com/mctc/mctcapidoc/User.java
+++ src/main/java/com/mctc/mctcapidoc/User.java
@@ -15,4 +15,27 @@ public class User {
     @ApiModelProperty("用户年龄")
     private Integer age;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
