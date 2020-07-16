7943a4e3053c7eef648675baf37c71951216b09f
GongJun <gjuse@vip.qq.com>
2020-07-16 17:55:43 +0800

    Initial commit

---
+++ src/main/java/com/mctc/mctcapidoc/MctcApiDocApplication.java
@@ -0,0 +1,15 @@
+package com.mctc.mctcapidoc;
+
+import com.spring4all.swagger.EnableSwagger2Doc;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableSwagger2Doc
+@SpringBootApplication
+public class MctcApiDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MctcApiDocApplication.class, args);
+    }
+
+}
